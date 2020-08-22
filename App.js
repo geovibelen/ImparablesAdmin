04d75428713a04623dd5fedafbc091d929006303
geovibelen <geovibelen2@hotmail.com>
2020-08-22 08:31:24 -0500

    Creacion carpeta Screens y componente Preguntas

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Preguntas } from "./Screens/Preguntas";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function TabHome() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="PeguntasScreen" component={Preguntas}></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ImparablesAdmin">
+        <Stack.Screen name="ImparablesAdmin" component={TabHome}></Stack.Screen>
+        <Stack.Screen
+          name="PreguntasScreen"
+          component={Preguntas}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
