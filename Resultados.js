7afe8921b8bd925cba1f7010df427f7584005499
Fe-59 <fega_cruz@outlook.com>
2020-08-24 08:12:14 -0500

    Componente Resultados

---
+++ Resultados.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+
+export class Resultados extends Component{
+render(){
+	return <View styles={main.styles}>
+		<Text>Resultados</Text>
+		</View>
+}
+
+}
+
+const styles = StyleSheet.create({
+	  main:{ 
+	  flex: 1,
+	  borderBottomColor: "#000",	  
+	  },
+	  });
+
+
