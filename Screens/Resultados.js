a4105464b965f49a44ae5780c5c3b16866c74d96
geovibelen <geovibelen2@hotmail.com>
2020-08-24 10:11:39 -0500

    Solucion error en Resultados

--- Screens/Resultados.js
+++ Screens/Resultados.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export class Resultados extends Component{
-render(){
-	return <View styles={main.styles}>
-		<Text>Resultados</Text>
-		</View>
-};
-
-};
+export class Resultados extends Component {
+  render() {
+    return (
+      <View styles={styles.main}>
+        <Text>Resultados</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-	  main:{ 
-	  flex: 1,
-	  borderBottomColor: "#000",	  
-	  },
-	  });
-
-
+  main: {
+    flex: 1,
+    borderBottomColor: "#000",
+  },
+});
