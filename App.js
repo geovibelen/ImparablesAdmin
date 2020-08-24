c9c3303dfb1741541d14a16efdebacfb1f764526
Fe-59 <fega_cruz@outlook.com>
2020-08-24 08:44:15 -0500

    Resultados Screen incluido en el Navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Preguntas } from "./Screens/Preguntas";
 import { Visitas } from "./Screens/Visitas";
+import { Resultados } from "./Screens/Resultados";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -14,6 +15,7 @@ function TabHome() {
     <Tab.Navigator>
       <Tab.Screen name="PeguntasScreen" component={Preguntas}></Tab.Screen>
       <Tab.Screen name="VisitasScreen" component={Visitas}></Tab.Screen>
+      <Tab.Screen name="ResultadosScreen" component={Resultados}></Tab.Screen>
     </Tab.Navigator>
   );
 }
@@ -21,17 +23,28 @@ function TabHome() {
 export default function App() {
   return (
     <NavigationContainer>
+
       <Stack.Navigator initialRouteName="ImparablesAdmin">
-        <Stack.Screen name="ImparablesAdmin" component={TabHome}></Stack.Screen>
-        <Stack.Screen
-          name="PreguntasScreen"
-          component={Preguntas}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="VisitasScreen"
-          component={Visitas}
-        ></Stack.Screen>
+
+          <Stack.Screen name="ImparablesAdmin" component={TabHome}></Stack.Screen>
+          
+          <Stack.Screen
+            name="PreguntasScreen"
+            component={Preguntas}
+          ></Stack.Screen>
+
+          <Stack.Screen
+            name="VisitasScreen"
+            component={Visitas}
+          ></Stack.Screen>
+
+          <Stack.Screen
+            name="ResultadosScreen"
+            component={Resultados}
+          ></Stack.Screen>
+
       </Stack.Navigator>
+
     </NavigationContainer>
   );
 }
